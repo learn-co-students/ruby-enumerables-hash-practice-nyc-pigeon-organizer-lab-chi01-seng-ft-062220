9a3d7305f41f3382939548f0078b8a27a7060809
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-29 14:03:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  data.each do |top_key, second_key|
+   second_key.each do |characteristics, names|
+    names.each do |pigeon|
+      if result[pigeon] == nil
+      result[pigeon] = {}
+      end
+      if result[pigeon][top_key] == nil
+        result[pigeon][top_key] = []
+      end
+      result[pigeon][top_key] << characteristics.to_s
+    end
+  end
+end
+result
 end
