4bcf07a640877f9a130f3994509caa01d6950fbb
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-08 19:45:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_list = {}
+  data.each do |key, value|
+    value.each do |traits, pigeon_name|
+      pigeon_name.each do |name|
+        if new_list[name] == nil
+          new_list[name] = {}
+        end
+        if new_list[name][key] == nil
+          new_list[name][key] = []
+        end
+        new_list[name][key].push(traits.to_s)
+      end
+    end 
+  end
+  new_list
 end
