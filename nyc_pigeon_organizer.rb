b700a32a663ad7d29006082f614e99b092faed96
Seth Starr <starrseth@hotmail.com>
2020-06-19 00:21:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,55 +1,21 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
- new_pigeon_data = Hash.new
-
-  #start iterating over the current hash
-  count = 0
-  while count < data.keys.length
-
-      count1 = 0
-      while count1 < data[data.keys[count]].keys.length
-
-        val_count = 0
-        while val_count < data[data.keys[count]].values[count1].length do
-
-          #for each terminal name, get the info for this chain.
-          pige_name = data[data.keys[count]].values[count1][val_count].to_s
-          new_pige_atr = data.keys[count].to_sym
-          new_pige_value = data[data.keys[count]].keys[count1].to_s
-
-          #if the name has already been added, add the new data, otherwise add the new data to this name
-          if new_pigeon_data.include? pige_name
-
-            if new_pigeon_data[pige_name].include? new_pige_atr
-              new_values = Array.new
-              new_values << new_pigeon_data[pige_name].values
-              new_values << new_pige_value
-              new_pigeon_data[pige_name][new_pige_atr] = new_values.flatten
-
-            else
-              new_value = Array.new
-              new_value << new_pige_value
-              new_pigeon_data[pige_name][new_pige_atr] = new_value
-
-           end
-
-          else
-            new_values = Array.new
-            new_values.push(new_pige_value)
-            new_pigeon_data[pige_name] = {
-              new_pige_atr => new_values
-            }
-          end
-
-          val_count += 1
-        end
-
-        count1 += 1
-      end
-
-      count += 1
+ total_results = data.each_with_object({}) do |(key,value), f_array|
+   value.each do |index, name|
+     name.each do |inner_name|
+              if !f_array[inner_name]
+                f_array[inner_name] = {}
+                end 
+          if !f_array[inner_name][key]
+            !f_array[inner_name][key] = []
+            end 
+            #binding.pry 
+          f_array[inner_name][key].push(index.to_s)
+        end 
+      end 
     end
-  new_pigeon_data
+    #binding.pry
 end 
   # write your code here!
+
 
